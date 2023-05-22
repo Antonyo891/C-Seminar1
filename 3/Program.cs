994d1a2d21d42bd1de864e721421a87d45c7fa63
Anton <B_atonyo_S@bk.ru>
2023-05-22 22:24:02 +0300

    Целые числа в интервале

---
+++ 3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// /*5. Напишите программу, которая на вход принимает одно число (N),
+//а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Clear();
+Console.WriteLine("Введите число");
+int Number = int.Parse(Console.ReadLine());
+Console.Write($"Целые числа в интервале от {-Number} до {Number} ");
+ Console.WriteLine();
+for (int i = - Number; i<=Number ; i++)
+{
+   
+    Console.Write($" {i} ");
+    
+}
+ Console.WriteLine();
+
